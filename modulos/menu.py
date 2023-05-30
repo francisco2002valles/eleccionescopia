71b9362f80dcd1dba5d8b4fbe63aa3521f5829bf
Elpato99 <patozensualon@gmail.com>
2023-05-29 21:28:19 -0300

    menu

--- modulos/menu.py
+++ modulos/menu.py
@@ -1,21 +1,32 @@
-def menu():
-    print("Que tipo de resultados electorales desea ver?")
-    print("1- Presidente y Vicepresidente ")
-    print("2- Gobernador y Vicegobernador ")
-    print("3- Senadores")
-    print("4- Diputados")
+def menu(path):
+    import csv                      # Importamos csv y platform 
+    import platform
 
-    val = False
+    claseslist = []                   # Creamos una lista para todos los partidos
 
-    while val != True:
+    if platform.system() == 'Windows':
+        csvfile = path + "\csv\\clases.csv"           # Fix para linux
+    elif platform.system() == 'Linux':
+        csvfile = path + '/csv/clases.csv'
+
+    with open (csvfile, "r", ) as partidos:             # Leemos el archivo partidos.csv para la lista de partidos
+        reader = csv.reader(partidos)
+        for row in reader:
+            print(row)
+            claseslist.append(int(row[2]))
+    
+    print("Seleccione que resultado desea ver: ")
+    valido = False
+    while valido != True:                           # Chequeamos que el valor sea valido
         try:
-            cat = str(input(": ")) 
-            if cat != "1" and cat != "2" and cat != "3" and cat != "4":             # Validamos la categoria
-                val = False
-                print("Valor no valido")
-            else:
-                val = True
-        except ValueError:
-            print("Valor no valido")
-            val = False
-    return cat
+            resp = int(input("ID: "))               # Chequeamos que el valor sea un numero
+        except: 
+            valido = False
+        try:
+            claseslist.index(resp)                # Buscamos el valor en la lista de partidos
+            valido = True
+        except:
+            valido = False
+            print("Valor no valido")                # si no lo encuentra tira un error, usamos esto para definirlo como no valido
+
+    return resp
