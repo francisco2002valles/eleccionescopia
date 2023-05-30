0fc80632e8029bb34f1d1bf771ced1dcc2cbd159
Marcos_Villar <marcos.villar.mail@gmail.com>
2023-05-30 12:25:17 -0300

    votos coocking

--- modulos/votoswriter.py
+++ modulos/votoswriter.py
@@ -2,12 +2,30 @@ def csvfilewriter(path, resp):
     import csv
     import platform
 
+ 
     if platform.system() == 'Windows':
-        csvfile = path + "\csv\csv_regiones\\votos.csv"             # Fix para windows y linux
+        csvfile = path + (f"\csv\\")             # Fix para windows y linux
     elif platform.system() == 'Linux':
-        csvfile = path + '/csv/csv_regiones/votos.csv'
+        csvfile = path + '/csv/'
 
-    with open (csvfile, "r") as votos:
-        csvreader = csv.reader(votos)
-        for row in csvreader
+    if platform.system() == 'Windows':
+        csvfile2 = path + (f"\csv\\csv_regiones\csv_" )             # Fix para windows y linux
+    elif platform.system() == 'Linux':
+        csvfile2 = path + '/csv/csv_regiones/csv_'
+
+    csvprov = csvfile + "provincias.csv"
+    csvvot = csvfile + "votos.csv"
+
+    with open (csvfile, "r") as provincias:
+        csvreader = csv.reader(provincias)
+        for row in provincias:
+            csvfilereg = ( csvfile2 + row[0] + ".csv")
+            with open(csvfilereg, "w", ) as provwrite:
+                csvwriter = csv.writer(provwrite)
+                csvwriter.writerow("test")
+            
+    
+            
+    
+            
 
